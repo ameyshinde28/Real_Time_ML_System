9ca036b0f85cb85ca868a1987fa718d97899d805
ameyshinde28 <aammeeyy28@gmail.com>
2024-11-25 15:30:42 +0000

    Prepare price_predictor for deployment to Quix  Cloud
    > Streamlined the Makefile
    > Containerized the trainng script and the API
    > Added a .dockerignore file
    
    Comment: The current docker  file is not optimized. Pending...

--- services/price_predictor/src/config.py
+++ services/price_predictor/src/config.py
@@ -1,5 +1,33 @@
 from pydantic_settings import BaseSettings
 
+# class TrainingConfig(BaseSettings):
+#     feature_view_name: str 
+#     feature_view_version: int
+#     ohlc_window_sec: int
+#     forecast_steps: int
+#     ml_model_status: str
+#     feature_group_name: str
+#     feature_group_version: int
+#     product_id:str
+#     last_n_days: int
+#     n_search_trials: int
+#     n_splits: int
+#     last_n_minutes: int
+
+#     class Config:
+#         env_file = "training.env"
+
+# class PredictionConfig(BaseSettings):
+#     feature_view_name: str 
+#     feature_view_version: int
+#     ohlc_window_sec: int
+#     forecast_steps: int
+#     ml_model_status: str
+#     api_supported_product_ids: list[str]
+
+#     class Config:
+#         env_file = "prediction.env"
+
 class AppConfig(BaseSettings):
     feature_view_name: str 
     feature_view_version: int
@@ -36,5 +64,7 @@ class CometConfig(BaseSettings):
 
   
 config = AppConfig()
+# training_config = TrainingConfigConfig()
+# prediction_config = PredictionConfig()
 hopsworks_config = HopsworksConfig()
 comet_config=CometConfig()
